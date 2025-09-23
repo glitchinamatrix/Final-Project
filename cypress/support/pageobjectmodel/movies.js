da2d836834787c17a4a947d58e69d1595177679c
Megha <MeghaManohar.Mahajan@cognizant.com>
2025-09-23 09:07:57 +0530

    Commited by sweta

--- cypress/support/pageobjectmodel/movies.js
+++ cypress/support/pageobjectmodel/movies.js
@@ -17,3 +17,4 @@ class MoviesPage {
 }
 
 export default new MoviesPage();
+ 
--- cypress/support/pageobjectmodel/sports.js
+++ cypress/support/pageobjectmodel/sports.js
@@ -6,7 +6,7 @@ class SportsPage {
   searchSports() {
     cy.get('#master-header > .dds-px-3 > .dds-mt-3 > .dds-w-full > .dds-h-11 > .dds-inline-block')
       .type('sports{enter}');
-      cy.get('[aria-label="All"] > .dds-text-inverse').click();
+      // cy.get('[aria-label="All"] > .dds-text-inverse').click();
       cy.get('[aria-label="Events"] > .dds-text-inverse').click();
   }
 
@@ -26,6 +26,6 @@ class SportsPage {
       return text.includes('vs') || text.includes('sports');
     });
   }
-}
+} 
 
 export default new SportsPage();
